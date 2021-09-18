d5d0041cd6eb080e19d6fbbb2c00171afd83be21
waketakuo <takuo.wale.3y@stu.hosei.ac.jp>
2021-09-18 12:36:22 +0900

    session

--- main.go
+++ main.go
@@ -1,21 +1,29 @@
 package main
 
-import "github.com/gin-gonic/gin"
-
-type Person struct {
-	ID string `uri:"id" binding:"required,uuid"`
-	Name string `uri:"name" binding:"required"`
-}
+import (
+	"github.com/gin-gonic/contrib/sessions"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	route := gin.Default()
-	route.GET("/:name/:id", func(c *gin.Context) {
-		var person Person
-		if err := c.ShouldBindUri(&person); err != nil {
-			c.JSON(400, gin.H{"msg": err})
-			return
+	r := gin.Default()
+
+	store := sessions.NewCookieStore([]byte("secret"))
+	r.Use(sessions.Sessions("mysession", store))
+
+	r.GET("/uncr", func(c *gin.Context) {
+		session := sessions.Default(c)
+		var count int
+		v := session.Get("count")
+		if v == nil {
+			count = 0
+		} else {
+			count = v.(int)
+			count += 1
 		}
-		c.JSON(200, gin.H{"name": person.Name, "uuid": person.ID})
+		session.Set("count", count)
+		session.Save()
+		c.JSON(200, gin.H{"count": count})
 	})
-	route.Run(":8080")
+	r.Run(":8080")
 }
