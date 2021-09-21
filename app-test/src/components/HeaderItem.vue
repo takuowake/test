c0a3823f0f84e49333193542eb6e0f6e036a743f
waketakuo <takuo.wale.3y@stu.hosei.ac.jp>
2021-09-21 12:15:59 +0900

    add-vue.js

---
+++ app-test/src/components/HeaderItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <header id="header">
+    <h1>This is the header.</h1>
+    <nav id="header-nav">
+      <ul>
+        <router-link tag="li" id="home-nav" to="/"exact><a>ホーム</a></router-link>
+        <router-link tag="li" id="game-nav" to="/game"><a>ゲーム</a></router-link>
+        <router-link tag="li" id="info-nav" to="/info"><a>情報</a></router-link>
+        <router-link tag="li" id="gift-nav" to="/gift"><a>お土産</a></router-link>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
