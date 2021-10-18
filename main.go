d50ca3a6c08ffc0dbeb44da99655defcc1a9a91e
waketakuo <takuo.wale.3y@stu.hosei.ac.jp>
2021-10-18 11:52:42 +0900

    var & const

--- main.go
+++ main.go
@@ -1,25 +1,44 @@
 package main
 
-import "github.com/golang/protobuf/proto"
+import "fmt"
 
-...
-package helloworld
-...
+//You can declare "var" outside the function.
+var (
+	i int = 1
+	f64 float64 = 1.2
+	s string = "test"
+	t, f bool = true, false
+)
 
-type HelloRequest struct {
-	Name string `protobuf:"bytes,1,opt,name=name" json:"name,omitempty"`
+func Var() {
+	fmt.Println(i, f64, s, t, f)
 }
 
-func (m *HelloRequest) Reset() { *m = HelloRequest{} }
-func (m *HelloRequest) String() { return proto.CompactTextString(m) }
-func (*HelloRequest) ProtoMessage() {}
-func (*HelloRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{0} }
-
-func (m *HelloRequest) GetName() string {
-	if m != nil {
-		return m.Name
-	}
-	return ""
+//You cannot declare ":=" outside the function.
+func foo() {
+    xi := 1
+    xi = 2
+    var xf32 float32 = 1.2
+    xs := "test"
+    xt, xf := true, false
+    fmt.Println(xi, xf32, xs, xt, xf)
+    fmt.Printf("%T\n", xf32)
+    fmt.Printf("%T\n", xi)
 }
-...
 
+func Const() {
+	const (
+		Pi = 3.14
+		Username = "test_user"
+		Password = "test_pass"
+	)
+	var maximum_var int = 9223372036854775807
+	const maximum_const = 9223372036854775807 + 1
+	fmt.Println(Pi, Username, Password, maximum_var, maximum_const-1)
+}
+
+func main() {
+	Var()
+	foo()
+	Const()
+}
